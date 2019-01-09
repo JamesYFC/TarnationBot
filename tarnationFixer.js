5788f75a2ffa9bbbf1169aeb0cce31e2f86ea9f1
James <jyfchen57@yahoo.com.au>
2019-01-09 15:18:40 +1000

    Update tarnationFixer.js

--- tarnationFixer.js
+++ tarnationFixer.js
@@ -5,6 +5,7 @@ const censoredPhrases = require("./data/censored-phrases.json")
 module.exports.Fixer = class Fixer {
     constructor() {}
     
+    //TODO: return bad phrase info for use in replacement function, so we don't repeat work
     checkForBadPhrases(string) {
         let result = string.toLowerCase();
         for (let i = 0; i < Object.keys(censoredPhrases).length; i++)
